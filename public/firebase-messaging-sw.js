18fdf7a08210bf744d9b154bc1634543bab21377
ytlim <jesusloveyt@gmail.com>
2022-06-11 10:45:22 +0900

    public

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 const config =  { 
 	apiKey: "AIzaSyDCfpbbmTZsQwfbLWp-zcsVZhdBlFlZONw",
 	authDomain: "hauspital-v1.firebaseapp.com",
-//	databaseURL:  "https://imap-push-server.firebaseio.com", 
+    databaseURL: "https://hauspital-v1-default-rtdb.firebaseio.com",
 	projectId: "hauspital-v1",
     storageBucket: "hauspital-v1.appspot.com",
     messagingSenderId: "45566089485",
